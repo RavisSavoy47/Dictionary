a83b01eab5de2cfdfbb400bfb010660e3b1d24da
ravis <ravissavoy49@gmail.com>
2022-02-11 11:44:53 -0600

    made item struct private

--- Dictionary.h
+++ Dictionary.h
@@ -1,10 +1,4 @@
 #pragma once
-template<typename TKey, typename TValue>
-struct Item
-{
-	TKey itemKey;
-	TValue itemValue;
-};
 
 template<typename TKey, typename TValue>
 class Dictionary
@@ -75,7 +69,82 @@ public:
 
 private:
 
-	Item<TKey, TValue>* m_items = nullptr;
+	struct Item
+	{
+		TKey itemKey;
+		TValue itemValue;
+	};
+
+	Item* m_items = nullptr;
 
 	int m_count = 0;
 };
+
+template<typename TKey, typename TValue>
+inline Dictionary<TKey, TValue>::Dictionary()
+{
+}
+
+template<typename TKey, typename TValue>
+inline Dictionary<TKey, TValue>::Dictionary(const Dictionary<TKey, TValue>& other)
+{
+}
+
+template<typename TKey, typename TValue>
+inline Dictionary<TKey, TValue>::~Dictionary()
+{
+}
+
+template<typename TKey, typename TValue>
+inline void Dictionary<TKey, TValue>::clear()
+{
+
+}
+
+template<typename TKey, typename TValue>
+inline bool Dictionary<TKey, TValue>::containsKey(const TKey object) const
+{
+	return false;
+}
+
+template<typename TKey, typename TValue>
+inline bool Dictionary<TKey, TValue>::containsTValue(const TValue object) const
+{
+	return false;
+}
+
+template<typename TKey, typename TValue>
+inline void Dictionary<TKey, TValue>::addItem(const TKey& key, const TValue& value) const
+{
+
+}
+
+template<typename TKey, typename TValue>
+inline bool Dictionary<TKey, TValue>::tryGetValue(const TKey key, const TValue& value) const
+{
+	return false;
+}
+
+template<typename TKey, typename TValue>
+inline bool Dictionary<TKey, TValue>::remove(const TKey key)
+{
+	return false;
+}
+
+template<typename TKey, typename TValue>
+inline bool Dictionary<TKey, TValue>::remove(const TKey key, TValue& value)
+{
+	return false;
+}
+
+template<typename TKey, typename TValue>
+inline int Dictionary<TKey, TValue>::getCount() const
+{
+	return 0;
+}
+
+template<typename TKey, typename TValue>
+inline const Dictionary<TKey, TValue>& Dictionary<TKey, TValue>::operator=(const Dictionary<TKey, TValue> other)
+{
+	// TODO: insert return statement here
+}
