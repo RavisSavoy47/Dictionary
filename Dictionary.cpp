a41169ff480ef91ae414631f47c4c33030759c2f
ravis <ravissavoy49@gmail.com>
2022-02-14 12:57:03 -0600

    added checks for remove and add

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -3,5 +3,16 @@
 
 int main()
 {
-	
+	Dictionary<char, int> dictionary = Dictionary<char, int>();
+
+
+	dictionary.addItem('a', 4);
+	//dictionary.addItem('a', 4);
+
+	int x = 10;
+	dictionary.remove(20, x);
+
+
+	if(dictionary.containsTValue(4))
+	std::cout << "its here " << x;
 }
--- Dictionary.h
+++ Dictionary.h
@@ -64,7 +64,7 @@ public:
 	/// <returns></returns>
 	int getCount()const;
 
-	Dictionary<TKey, TValue>& operator=(const Dictionary<TKey, TValue> other)const;
+	const Dictionary<TKey, TValue>& operator=(Dictionary<TKey, TValue> other);
 
 	TValue operator[](const TKey key);
 
@@ -107,7 +107,7 @@ inline void Dictionary<TKey, TValue>::clear()
 	for (int i = 0; i < getCount(); i++)
 	{
 		//removes the items key and value
-		remove(m_items->itemKey, m_items->itemValue);
+		remove(m_items[i].itemKey, m_items[i].itemValue);
 	}
 	//resets the count
 	m_count = 0;
@@ -162,6 +162,11 @@ inline bool Dictionary<TKey, TValue>::tryGetValue(const TKey key, TValue& value)
 template<typename TKey, typename TValue>
 inline void Dictionary<TKey, TValue>::addItem(const TKey& key, const TValue& value)
 {
+	// If there are no items in the list...
+	if (getCount() <= 0)
+		// ...return false.
+		return;
+
 	Item* temp = new Item[getCount() + 1];
 
 	for (int i = 0; i < getCount(); i++)
@@ -182,14 +187,23 @@ inline void Dictionary<TKey, TValue>::addItem(const TKey& key, const TValue& val
 template<typename TKey, typename TValue>
 inline bool Dictionary<TKey, TValue>::remove(const TKey key)
 {
+	// If there are no items in the list...
+	if (getCount() <= 0)
+		// ...return false.
+		return false;
+
+	//creates a temp array
 	Item* temp = new Item[getCount() - 1];
 	int j = 0;
 	bool itemIsRemoved = false;
 
+	//iterates through the items
 	for (int i = 0; i < getCount(); i++)
 	{
+		//if the item key is not a key
 		if (m_items[i].itemKey != key)
 		{
+			//makes the temp array equal to the items
 			temp[j] = m_items[i];
 			j++;
 		}
@@ -197,6 +211,7 @@ inline bool Dictionary<TKey, TValue>::remove(const TKey key)
 			itemIsRemoved = true;	
 	}
 
+	//set the items to be the temp items
 	m_items = temp;
 	m_count--;
 
@@ -206,24 +221,35 @@ inline bool Dictionary<TKey, TValue>::remove(const TKey key)
 template<typename TKey, typename TValue>
 inline bool Dictionary<TKey, TValue>::remove(const TKey key, TValue& value)
 {
+	// If there are no items in the list...
+	if (getCount() <= 0)
+		// ...return false.
+		return false;
+
+	//creates a temp array of items 
 	Item* temp = new Item[getCount() - 1];
 	int j = 0;
 	bool itemIsRemoved = false;
 
+	//iterates through the items
 	for (int i = 0; i < getCount(); i++)
 	{
+		//if the item key is not a key
 		if (m_items[i].itemKey != key)
 		{
+			//makes the temp array equal to the items
 			temp[j] = m_items[i];
 			j++;
 		}
 		else
 		{
+			//sets value to be the items key if the item is removed
+			value = m_items->itemKey;
 			itemIsRemoved = true;
-			m_items->itemKey = value;
 		}
 	}
 
+	//set the items to be the temp items
 	m_items = temp;
 	m_count--;
 
@@ -231,18 +257,29 @@ inline bool Dictionary<TKey, TValue>::remove(const TKey key, TValue& value)
 }
 
 template<typename TKey, typename TValue>
-inline Dictionary<TKey, TValue>& Dictionary<TKey, TValue>::operator=(const Dictionary<TKey, TValue> other) const
+inline const Dictionary<TKey, TValue>& Dictionary<TKey, TValue>::operator=(Dictionary<TKey, TValue> other)
 {
-	for (int i = 0; i < other.m_count; i++)
-		m_items[i] = other.m_items[i];
+	//iterates through the items
+	for (int i = 0; i < other.getCount(); i++)
+		//adds items based on the others items 
+		addItem(other.m_items[i].itemKey, other.m_items[i].itemValue);
 
-	m_count = other.m_count;
+	//sets the count to be the other count
+	m_count = other.getCount();
 
-	return this;
+	return other;
 }
 
 template<typename TKey, typename TValue>
 inline TValue Dictionary<TKey, TValue>::operator[](const TKey key)
 {
+	TValue value;
+	// If the key can be found
+	if (tryGetValue(key, value))
+		// return the value found
+		return value;
+	// If there is no key found
+	else
+		//return an empty value
 	return TValue();
 }
