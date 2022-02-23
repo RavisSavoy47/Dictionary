404bdfb9f7652fb3f9af34f2cb236d0bde976bc0
ravis <ravissavoy49@gmail.com>
2022-02-23 17:00:26 -0600

    updating operators =

--- Dictionary.h
+++ Dictionary.h
@@ -113,13 +113,8 @@ inline int Dictionary<TKey, TValue>::getCount() const
 template<typename TKey, typename TValue>
 inline void Dictionary<TKey, TValue>::clear()
 {
-	//iterates though the items
-	for (int i = 0; i < getCount(); i++)
-	{
-		//removes the items key and value
-		remove(m_items[i].itemKey, m_items[i].itemValue);
-	}
-	//resets the count
+	//Deletes the items to clear the dictionary
+	delete m_items;
 	m_count = 0;
 }
 
@@ -237,10 +232,9 @@ inline bool Dictionary<TKey, TValue>::remove(const TKey key, TValue& value)
 
 	//creates a temp array of items 
 	Item* temp = new Item[getCount() - 1];
-	bool itemIsRemoved = false;
 
 	//iterates through the items
-	for (int i = 0; i < getCount(); i++)
+	for (int i = 0; i < getCount() - 1; i++)
 	{
 		//if the item key is not a key
 		if (m_items[i].itemKey != key)
@@ -252,25 +246,26 @@ inline bool Dictionary<TKey, TValue>::remove(const TKey key, TValue& value)
 		{
 			//sets value to be the items key if the item is removed
 			value = m_items[i].itemValue;
-			itemIsRemoved = true;
 		}
 	}
-
+	delete m_items;
 	//set the items to be the temp items
 	m_items = temp;
 	m_count--;
 
-	return itemIsRemoved;
+	return true;
 }
 
 template<typename TKey, typename TValue>
 inline const Dictionary<TKey, TValue>& Dictionary<TKey, TValue>::operator=(const Dictionary<TKey, TValue> other)
 {
+
 	//iterates through the items
 	for (int i = 0; i < other.getCount(); i++)
+	{
 		//adds items based on the others items 
 		addItem(other.m_items[i].itemKey, other.m_items[i].itemValue);
-
+	}
 	//sets the count to be the other count
 	m_count = other.getCount();
 
